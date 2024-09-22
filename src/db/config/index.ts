57003b0ed350eea215326fd518a8f5112b2534bf
Bagas Udi Sahsangka <udibagas@gmail.com>
2024-09-22 19:53:27 +0700

    update dockerfile

--- src/db/config/index.ts
+++ src/db/config/index.ts
@@ -3,4 +3,4 @@ import { MongoClient } from "mongodb";
 const uri = process.env.MONGODB_URI as string;
 const client = new MongoClient(uri);
 
-export const database = client.db("next-rmt-52");
+export const database = client.db(process.env.DB_NAME);
